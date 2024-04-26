6ccc283ceec1880f0a34bb9191a57a843685e8f0
guilherminho <gsouza2651@gmail.com>
2024-04-26 18:00:42 -0300

    Skeleton height fix

--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -114,7 +114,7 @@ export function SearchResults() {
         <ul className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 auto-rows-auto gap-y-7 gap-x-7 justify-items-center w-full">
           {Array.from({ length: 20 }).map((_, index) => (
             <li key={index} className="max-w-[185px] w-full space-y-3">
-              <Skeleton className="max-w-[185px] w-full h-[278px] rounded" />
+              <Skeleton className="max-w-[185px] w-full h-auto aspect-[0.7] rounded" />
               <Skeleton className="max-w-[150px] w-full h-5 rounded" />
             </li>
           ))}
